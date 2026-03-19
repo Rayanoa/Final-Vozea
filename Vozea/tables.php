0489c2ff686980aee05d0ec7bf465ac3f6b7983e
Rayanoa <rayanoaf@gmail.com>
2026-03-19 17:00:57 +0100

    Adding some things

--- Vozea/tables.php
+++ Vozea/tables.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Tables</title>
+    <title> Création des classes </title>
 
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -38,7 +38,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3"> Vozea <sup>2</sup></div>
             </a>
 
             <!-- Divider -->
@@ -379,8 +379,8 @@
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
+                                            <th> Nom de la classe</th>
+                                            <th> Promotion </th>
                                             <th>Office</th>
                                             <th>Age</th>
                                             <th>Start date</th>
