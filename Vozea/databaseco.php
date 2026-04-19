bb0f1d064a65d888bb7f92934ae0cd20e9645f82
Rayanoa <rayanoaf@gmail.com>
2026-04-19 13:47:35 +0200

    Ajout co bdd + navbar

--- Vozea/databaseco.php
+++ Vozea/databaseco.php
@@ -5,7 +5,7 @@ $password = "123456";
 $dbname = "bdvozea";
 
 try {
-  $conn = new PDO("mysql:host=$localhost;dbname=$bdvozea", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
